7ada81895f4306dffd20683c9731ece225e398f7
darkolol9 <hussein.s@doktorabc.com>
2025-02-08 14:06:47 +0200

    more color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,13 @@ return {
   -- Add gruvbox-material theme
   { "sainnhe/gruvbox-material" },
   { "sainnhe/everforest" },
+  { "navarasu/onedark.nvim" },
 
   -- Configure LazyVim to load gruvbox-material with light background
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "onedark",
     },
   },
 }
