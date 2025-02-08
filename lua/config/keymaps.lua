41d99a5bd8dfd30a8510a4d5287f531b51cb0561
darkolol9 <hussein.s@doktorabc.com>
2025-02-08 11:44:49 +0200

    moved keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+--
+vim.api.nvim_set_keymap("n", "<C-c>", '"+y', { noremap = true, silent = true }) -- Copy to clipboard
+vim.api.nvim_set_keymap("n", "<C-v>", '"+p', { noremap = true, silent = true }) -- Paste from clipboard
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true, silent = true }) -- Copy to clipboard in visual mode
+vim.api.nvim_set_keymap("v", "<C-v>", '"+p', { noremap = true, silent = true }) -- Paste from clipboard in visual mode
+-- Remap Ctrl+Z to undo
+vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true })
