cc9883c51d7b39ac2c16d48c6e1fefe225f1c146
darkolol9 <hussein.s@doktorabc.com>
2025-02-08 14:56:48 +0200

    new rules

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,12 @@ return {
   { "sainnhe/gruvbox-material" },
   { "sainnhe/everforest" },
   { "navarasu/onedark.nvim" },
-
+  { "scottmckendry/cyberdream.nvim" },
   -- Configure LazyVim to load gruvbox-material with light background
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "gruvbox-material",
     },
   },
 }
