05f3fddae232868b44af391cb8176025ecdb7bb8
darkolol9 <hussein.s@doktorabc.com>
2025-02-10 21:44:56 +0200

    yay line swaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,20 @@ vim.api.nvim_set_keymap("v", "<C-v>", '"+p', { noremap = true, silent = true }) 
 vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true })
 
 
+-- Swap the current line with the one above it (Alt + Up)
+vim.api.nvim_set_keymap('n', '<A-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- Swap the current line with the one below it (Alt + Down)
+vim.api.nvim_set_keymap('n', '<A-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+
+-- Swap selected lines with the one above it (Alt + Up in Visual Mode)
+vim.api.nvim_set_keymap('v', '<A-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Swap selected lines with the one below it (Alt + Down in Visual Mode)
+vim.api.nvim_set_keymap('v', '<A-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+
+
 -- Override the default keymap for cwd wide grep
 vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle<cr>', { noremap = true, silent = true })
