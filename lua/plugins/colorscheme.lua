7c5437c0419b2dcf6332ac18c6849e06f2116fb6
darkolol9 <hussein.s@doktorabc.com>
2025-02-14 16:02:14 +0200

    breadcrumbs and explorer

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,11 +5,12 @@ return {
   { "navarasu/onedark.nvim" },
   { "scottmckendry/cyberdream.nvim" },
   { "folke/tokyonight.nvim" },
+  { "cpea2506/one_monokai.nvim" },
   -- Configure LazyVim to load gruvbox-material with light background
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "one_monokai",
     },
   },
 }
