c4b11d7480a8b515977fb55128d2cbb862d6f35e
darkolol9 <darkolol9@gmail.com>
2025-11-07 11:37:42 +0200

    changed smart find files and grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
       local telescope = require("telescope")
 
       -- Set key mappings after Telescope is loaded
-      vim.api.nvim_set_keymap('n', '<Leader><Leader>', '<Cmd>Telescope find_files<CR>', { noremap = true, silent = true })  -- search in the current directory
+      -- vim.api.nvim_set_keymap('n', '<Leader><Leader>', '<Cmd>Telescope find_files<CR>', { noremap = true, silent = true })  -- search in the current directory
       -- vim.api.nvim_set_keymap('n', '<Leader>fF', '<Cmd>Telescope find_files cwd=~<CR>', { noremap = true, silent = true })  -- search everywhere
 
       -- You can also call telescope.setup() here to ensure it's configured
