589e66c541583473cecaa42c15a2415383202bfd
darkolol9 <darkolol9@gmail.com>
2025-11-14 08:49:01 +0200

    yes sirrrrr new ai comp

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ require("config.lazy")
 -- console.log(:m '>+1<CR>gv=gv
 
 -- //set theme
--- vim.cmd "colorscheme darkblue"
-require("tokyonight").load({ style = "night" })
+vim.cmd "colorscheme citruszest"
+-- require("tokyonight").load({ style = "night" })
 print("setting theme")
 
 
@@ -31,9 +31,8 @@ local groups = {
 }
 
 for _, group in ipairs(groups) do
-  vim.api.nvim_set_hl(0, group, { bg = "none" })
+  -- vim.api.nvim_set_hl(0, group, { bg = "none" })
 end
-
 
 return {
   -- nvim-lspconfig for Language Server Protocol
@@ -46,7 +45,7 @@ return {
         on_attach = function(client, bufnr)
           client.server_capabilities.documentFormattingProvider = false
           -- Enable completion
-          require("cmp").setup.buffer({ completion = { autocomplete = false } })
+          -- require("cmp").setup.buffer({ completion = { autocomplete = false } })
           -- Set up some keymaps if desired (for LSP functions)
           vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true })
           vim.api.nvim_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true })
@@ -56,31 +55,32 @@ return {
   },
 
   -- nvim-cmp for autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        completion = {
-          autocomplete = false, -- Turn off auto popup on typing
-        },
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- Use vsnip (or your snippet engine)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(), -- Trigger autocomplete manually
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = {
-          { name = "lunasnip", priority = 10000 },
-          { name = "vsnip", priority = 900},
-          { name = "codeium", priority = 800 },
-        },
-      })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   config = function()
+  --     local cmp = require("cmp")
+  --     cmp.setup({
+  --       completion = {
+  --         autocomplete = false, -- Turn off auto popup on typing
+  --       },
+  --       snippet = {
+  --         expand = function(args)
+  --           vim.fn["vsnip#anonymous"](args.body) -- Use vsnip (or your snippet engine)
+  --         end,
+  --       },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ["<C-Space>"] = cmp.mapping.complete(), -- Trigger autocomplete manually
+  --         ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+  --       }),
+  --       sources = {
+  --         { name = "nvim_lsp", priority = 15000 },
+  --         { name = "lunasnip", priority = 10000 },
+  --         { name = "vsnip", priority = 900},
+  --         { name = "codeium", priority = 800 },
+  --       },
+  --     })
+  --   end,
+  -- },
 
   -- mason.nvim to handle external tools like language servers
   {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,7 +14,7 @@ return {
     config = function()
       require("nightfox").setup({
         options = {
-          transparent = false,  -- set true if you want transparent background
+          transparent = false, -- set true if you want transparent background
           terminal_colors = true,
           styles = {
             comments = "italic",
@@ -25,6 +25,50 @@ return {
       })
       vim.cmd("colorscheme terafox")
     end,
+  },
+  -- Lua
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      })
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      vim.cmd("colorscheme poimandres")
+    end,
+  },
+  {
+    "uloco/bluloco.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      -- your optional config goes here, see below.
+    end,
+  },
+  {
+    "vague-theme/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
+      })
+      vim.cmd("colorscheme vague")
+    end,
+  },
+  {
+    "zootedb0t/citruszest.nvim",
+    lazy = false,
+    priority = 1000,
   },
   -- Tell LazyVim which colorscheme to use by default
   {
