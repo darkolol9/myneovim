bd2e7a112cff9a8ccfed79ec99b0e838f64c6694
darkolol9 <darkolol9@gmail.com>
2025-11-15 11:14:48 +0200

    more keybinds

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -6,6 +6,7 @@ return {
     dv.setup()
 
     vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Diffview Open" })
+    vim.keymap.set("n", "<leader>gH", ":DiffviewOpen<CR> HEAD", { desc = "Diffview vs HEAD" })
     vim.keymap.set("n", "<leader>gD", ":DiffviewClose<CR>", { desc = "Diffview Close" })
   end,
 }
