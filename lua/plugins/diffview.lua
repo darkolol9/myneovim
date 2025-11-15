124921f0ca411b95e59d9800b8b1cc53d1c13200
darkolol9 <darkolol9@gmail.com>
2025-11-15 11:07:45 +0200

    git diffview + removal of useless plugins

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+  "sindrets/diffview.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local dv = require("diffview")
+    dv.setup()
+
+    vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Diffview Open" })
+    vim.keymap.set("n", "<leader>gD", ":DiffviewClose<CR>", { desc = "Diffview Close" })
+  end,
+}
+
